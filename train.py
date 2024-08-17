9c991d2c7cf71d1b3aede18afbb5652e6a787f53
NatalieCheong <50092072+NatalieCheong@users.noreply.github.com>
2024-08-17 17:52:07 +0800

    Update train.py

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
 import skops.io as sio
 
-drug_df = pd.read_csv("Data/drug.csv")
+drug_df = pd.read_csv("Data/drug200.csv")
 drug_df = drug_df.sample(frac=1)
 drug_df.head(3)
 
